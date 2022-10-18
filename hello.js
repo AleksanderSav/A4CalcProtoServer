8e7571622e9b598fa97104fad6a926dd6c3f71a4
AleksanderSav <113454243+AleksanderSav@users.noreply.github.com>
2022-10-18 19:11:49 +0300

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
-
+require("dotenv").config();
+const router = require("./router/indexRouter");
 
 app.use("/", express.static("./public"));
-
+app.use(express.json());
+// app.use("/api", () => {
+//    console.log("ok");
+// });
+app.use("/api", router);
 
 app.listen(process.env.APP_PORT, process.env.APP_IP);
 app.listen((err) => {
-    if (err) {
-        return console.log("something bad happened", err);
-    }
-    console.log(`server is listening ${process.env.APP_IP},${process.env.APP_PORT}`);
+   if (err) {
+      return console.log("something bad happened", err);
+   }
+   console.log(
+      `server is listening ${process.env.APP_IP},${process.env.APP_PORT}`
+   );
 });
