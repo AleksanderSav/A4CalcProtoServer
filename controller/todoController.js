8c04fdea83d924415721846a3e8b98fc7aa354a6
AleksanderSav <113454243+AleksanderSav@users.noreply.github.com>
2022-10-20 12:39:43 +0300

    test

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,25 +1,26 @@
-class TodoController{
-    async todoGet(){
-        try{
-            console.log("todoGet")
-        }catch (e){
-            console.log(e)
-        }
-    }
-    async todoPost(){
-        try{
-            console.log("todoPost")
-        }catch (e){
-            console.log(e)
-        }
-    }
-    async todoRemove(){
-        try{
-            console.log("todoRemove")
-        }catch (e){
-            console.log(e)
-        }
-    }
+class TodoController {
+   async todoGet(req, res) {
+      try {
+         res.json({ message: "response ok" });
+         console.log("todoGet1");
+      } catch (e) {
+         console.log(e);
+      }
+   }
+   async todoPost() {
+      try {
+         console.log("todoPost");
+      } catch (e) {
+         console.log(e);
+      }
+   }
+   async todoRemove() {
+      try {
+         console.log("todoRemove");
+      } catch (e) {
+         console.log(e);
+      }
+   }
 }
 
-module.exports = new TodoController()
+module.exports = new TodoController();
