2a14979b3dd39c32986be2591aa87de44dfa560d
Aleksandersie <aleksander.sie@gmail.com>
2022-10-21 08:43:21 +0300

    test post

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,15 +1,22 @@
+const {ToDo} = require("../dbModels/dbModels")
+
 class TodoController {
    async todoGet(req, res) {
       try {
-         res.json({ message: "response ok" });
-         console.log("todoGet1");
+          res.json({ message: "response ok!" });
+          console.log("todoGet1");
+
+
       } catch (e) {
          console.log(e);
       }
    }
-   async todoPost() {
+   async todoPost(req,res) {
       try {
-         console.log("todoPost");
+         const {message,randomNumber} = req.body
+         const toDo = await ToDo.create({message,randomNumber})
+         return res.json(toDo)
+
       } catch (e) {
          console.log(e);
       }
