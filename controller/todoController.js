0d41a18c37dbb2d33ea204ccda30ea0067426dc4
AleksanderSav <113454243+AleksanderSav@users.noreply.github.com>
2022-10-21 19:12:16 +0300

    db work

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,29 +1,28 @@
-const {ToDo} = require("../dbModels/dbModels")
+const { ToDo } = require("../dbModels/dbModels");
 
 class TodoController {
    async todoGet(req, res) {
       try {
-          res.json({ message: "response ok!" });
-          console.log("todoGet1");
-
-
+         const toDo = await ToDo.findAll();
+         res.json(toDo);
       } catch (e) {
          console.log(e);
       }
    }
-   async todoPost(req,res) {
+   async todoPost(req, res) {
       try {
-         const {message,randomNumber} = req.body
-         const toDo = await ToDo.create({message,randomNumber})
-         return res.json(toDo)
-
+         const { message, randomNumber } = req.body;
+         const toDo = await ToDo.create({ message, randomNumber });
+         return res.json(toDo);
       } catch (e) {
          console.log(e);
       }
    }
-   async todoRemove() {
+   async todoRemove(req, res) {
       try {
-         console.log("todoRemove");
+         const { randomNumber } = req.body;
+         const toDo = await ToDo.destroy({ where: { randomNumber } });
+         return res.json(toDo);
       } catch (e) {
          console.log(e);
       }
