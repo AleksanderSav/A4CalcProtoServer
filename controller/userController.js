32905f931e0c47c98794764720675aa4ceec9a42
Aleksandersie <aleksander.sie@gmail.com>
2022-11-19 13:28:31 +0300

    Order owner added

--- controller/userController.js
+++ controller/userController.js
@@ -2,116 +2,125 @@ const { User } = require("../dbModels/dbModels");
 const jwt = require("jsonwebtoken");
 
 const generateJWT = (id, email, alias, role, priceCategory) => {
-   return jwt.sign({ id, email, alias, role, priceCategory }, "1234", {
-      expiresIn: "24h",
-   });
+  return jwt.sign({ id, email, alias, role, priceCategory }, "1234", {
+    expiresIn: "24h",
+  });
 };
 
 class UserController {
-   /////////////////////////////////////////////
-   async getOneUser(req, res) {
-      try {
-         const { id } = req.params;
-         const findOne = await User.findOne({ where: { id } });
-         if (findOne === null) {
-            return res.json("User is not found");
-         }
-         return res.json(findOne);
-      } catch (e) {
-         console.log(e);
+  /////////////////////////////////////////////
+  async getOneUser(req, res) {
+    try {
+      const { id } = req.params;
+      const findOne = await User.findOne({ where: { id } });
+      if (findOne === null) {
+        return res.json("User is not found");
       }
-   }
-   ///////////////////////////////////////
-   async getAllUsers(req, res) {
-      try {
-         const allUsers = await User.findAll();
-         return res.json(allUsers);
-      } catch (e) {
-         console.log(e);
+      return res.json(findOne);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  ///////////////////////////////////////
+  async getAllUsers(req, res) {
+    try {
+      const allUsers = await User.findAll();
+      return res.json(allUsers);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  /////////////////////////////////////
+  async getAllCustomers(req, res) {
+    try {
+      const allUsers = await User.findAll({ where: { role: "customer" } });
+      return res.json(allUsers);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  /////////////////////////////////////////
+  async createUser(req, res) {
+    try {
+      const { email, password, alias, role, priceCategory } = req.body;
+      if (!email || !password) {
+        return res.json({ message: "Enter login and password" });
       }
-   }
-   /////////////////////////////////////////
-   async createUser(req, res) {
-      try {
-         const { email, password, alias, role, priceCategory } = req.body;
-         if (!email || !password) {
-            return res.json({ message: "Enter login and password" });
-         }
-         const candidate = await User.findOne({ where: { email } });
-         if (candidate) {
-           return  res.json({ message: "User already exist " });
-         }
+      const candidate = await User.findOne({ where: { email } });
+      if (candidate) {
+        return res.json({ message: "User already exist " });
+      }
 
-         const user = await User.create({
-            email,
-            password,
-            alias,
-            role,
-            priceCategory,
-         });
-         const token = generateJWT(
-            user.id,
-            user.email,
-            user.alias,
-            user.role,
-            user.priceCategory
-         );
-         return res.json({ token });
-      } catch (e) {
-         console.log(e);
+      const user = await User.create({
+        email,
+        password,
+        alias,
+        role,
+        priceCategory,
+      });
+      const token = generateJWT(
+        user.id,
+        user.email,
+        user.alias,
+        user.role,
+        user.priceCategory
+      );
+      return res.json({ token });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  ////////////////////////////////////////
+  async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
+      console.log(req.body);
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        res.json({ message: "User not found" });
       }
-   }
-   ////////////////////////////////////////
-   async loginUser(req, res) {
-      try {
-         const { email, password } = req.body;
-         console.log(req.body)
-         const user = await User.findOne({ where: { email } });
-         if (!user) {
-            res.json({ message: "User not found" });
-         }
-         if (password !== user.password) {
-            res.json({ message: "Password error" });
-         }
-         const token = generateJWT(
-            user.id,
-            user.email,
-            user.alias,
-            user.role,
-            user.priceCategory
-         );
-         res.json({ token });
-      } catch (e) {
-         console.log(e);
+      if (password !== user.password) {
+        res.json({ message: "Password error" });
       }
-   }
-   ////////////////////////////////////////////
-   async updateUser(req, res) {
-      try {
-         const { email, password, alias, role, priceCategory } = req.body;
-         const user = await User.findOne({ where: { email } });
-         await user.update({
-            email: email,
-            password: password,
-            alias: alias,
-            role: role,
-            priceCategory: priceCategory,
-         });
-         return res.json(user);
-      } catch (e) {
-         console.log(e);
-      }
-   }
-   /////////////////////////////////////////////////
-   async removeUser(req, res) {
-      try {
-         const { email } = req.body;
-         const user = await User.destroy({ where: { email } });
-         return res.json(user);
-      } catch (e) {
-         console.log(e);
-      }
-   }
+      const token = generateJWT(
+        user.id,
+        user.email,
+        user.alias,
+        user.role,
+        user.priceCategory
+      );
+      res.json({ token });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  ////////////////////////////////////////////
+  async updateUser(req, res) {
+    try {
+      const { email, password, alias, role, priceCategory } = req.body;
+      const user = await User.findOne({ where: { email } });
+      await user.update({
+        email: email,
+        password: password,
+        alias: alias,
+        role: role,
+        priceCategory: priceCategory,
+      });
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  /////////////////////////////////////////////////
+  async removeUser(req, res) {
+    try {
+      const { email } = req.body;
+      const user = await User.destroy({ where: { email } });
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
 
 module.exports = new UserController();
--- router/userRouter.js
+++ router/userRouter.js
@@ -4,6 +4,7 @@ const router = new Router();
 
 router.get("/getone/:id", userController.getOneUser);
 router.get("/getall", userController.getAllUsers);
+router.get("/getAllCustomers", userController.getAllCustomers);
 router.post("/", userController.createUser);
 router.post("/login", userController.loginUser);
 router.put("/", userController.updateUser); ///////
